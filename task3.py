921dacead76ac4fe380644b867c503b84bbe2bc9
wildcat5566 <b01611005@gmail.com>
2018-02-05 23:25:51 +0800

    Run on Raspberry Pi

---
+++ task3.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import time
+
+def CameraCapture():
+    cp0 = cv2.VideoCapture(0)
+    while 1:
+        ret0, frame0 = cp0.read()
+	cv2.imshow('Test0', frame0)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		cp0.release()
+
+def DetermineColor(img):
+    length, width, _ = img.shape
+    color = None
+    mono = []
+    
+    # TODO
+
+    # TODO: Set & apply thresholds for each channel
+
+    # TODO: Determine color
+
+    # TODO: 
+
+    return color, mono
+
+def markcircles(img, mono):
+    # No need to modify anything in this
+
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        cv2.circle(img, (i[0],i[1]),i[2],(0,0,0),2) 
+        cv2.circle(img, (i[0],i[1]),2,(255,255,255),3)
+    
+
+img = cv2.imread('./src/red3.jpg')
+mono, color = DetermineColor(img)
+
+markcircles(img, mono)
+print(color)
+
+cv2.imshow('Task3', img)
+cv2.imwrite('Task3_example.jpg', img)
