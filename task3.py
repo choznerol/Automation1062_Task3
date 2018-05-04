11369f9b54ac2209d43dfb36ea8d24d044cd9831
Szuyu Lin <b01611005@gmail.com>
2018-05-04 15:19:41 +0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import time
 
+"""
 def CameraCapture():
     cp0 = cv2.VideoCapture(0)
     while 1:
@@ -9,11 +10,12 @@ def CameraCapture():
 	cv2.imshow('Test0', frame0)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		cp0.release()
+"""
 
-def DetermineColor(img):
+def DetermineColor_FindCircles(img):
     length, width, _ = img.shape
     color = None
-    mono = []
+    circles = []
     
     # TODO
 
@@ -23,9 +25,9 @@ def DetermineColor(img):
 
     # TODO: find circles
 
-    return color, mono
+    return color, circles
 
-def markcircles(img, mono):
+def markcircles(img, circles):
     # No need to modify anything in this
 
     circles = np.uint16(np.around(circles))
@@ -34,11 +36,11 @@ def markcircles(img, mono):
         cv2.circle(img, (i[0],i[1]),2,(255,255,255),3)
     
 
-img = cv2.imread('./src/red3.jpg')
-mono, color = DetermineColor(img)
+image = cv2.imread('./src/red3.jpg')
+circles_found, color_found = DetermineColor_FindCircles(image)
 
-markcircles(img, mono)
-print(color)
+markcircles(image, circles_found)
+print(color_found)
 
-cv2.imshow('Task3', img)
-cv2.imwrite('Task3_example.jpg', img)
+cv2.imshow('Task3', image)
+cv2.imwrite('Task3_example.jpg', image)
